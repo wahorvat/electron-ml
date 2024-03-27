10399a51a61758e3312e6028ec6e0a7232e440f7
wahorvat <wahorvat@gmail.com>
2024-03-27 12:13:17 -0400

    e3nn layers

--- setup.py
+++ setup.py
@@ -15,8 +15,11 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires=r'>=3.8.0',
     install_requires=[
-        r'torch>=1.5.0',
+        r'torch>=1.8.0',
         r'numpy>=1.20.0'
+        r'sympy',
+        r'scipy',
+        r'opt_einsum_fx>=0.1.4'
     ],
     zip_safe=True
 )
